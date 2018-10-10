e88cbf165babd2acb5c7e9cc6de35b563ee29139
lpisces <iamalazyrat@gmail.com>
2018-10-10 16:11:15 +0800

    add download

--- handler/id_img.go
+++ handler/id_img.go
@@ -5,15 +5,90 @@ import (
 	//"github.com/labstack/gommon/log"
 	"github.com/lpisces/idcheck/config"
 	//"github.com/lpisces/idcheck/model"
+	"archive/zip"
+	"fmt"
 	"github.com/disintegration/imaging"
+	"github.com/labstack/gommon/log"
 	"image"
 	"image/color"
 	"io"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
+
+func HandleIDImageDownload(conf *config.Config) func(c echo.Context) error {
+	type (
+		Ret struct {
+			Status   int64
+			Msg      string
+			NotFound []string
+			Url      string
+		}
+	)
+
+	return func(c echo.Context) error {
+
+		r := Ret{
+			0,
+			"",
+			nil,
+			"",
+		}
+
+		v := c.QueryParams()
+
+		if !checkSign(v) {
+			r.Status = http.StatusUnauthorized
+			r.Msg = "invalid sign"
+			return c.JSON(http.StatusUnauthorized, r)
+		}
+
+		expire, err := strconv.ParseInt(v.Get("expire"), 10, 64)
+		if err != nil {
+			return err
+		}
+		if expire < time.Now().Unix() {
+			r.Status = http.StatusUnauthorized
+			r.Msg = "expired"
+			return c.JSON(http.StatusUnauthorized, r)
+		}
+
+		ids := c.FormValue("ids")
+		path := conf.IDImageUploadDir
+
+		id_arr := strings.Split(string(ids), ",")
+
+		var notFound, zipFiles []string
+		for _, id := range id_arr {
+			log.Info(id)
+			filename := path + "/" + id + "_merged.jpg"
+			log.Info(filename)
+			if _, err := os.Stat(filename); os.IsNotExist(err) {
+				notFound = append(notFound, id)
+				continue
+			}
+			zipFiles = append(zipFiles, filename)
+		}
+
+		output := fmt.Sprintf("%s/%s%s.zip", path, "ids_", time.Now().Format("20060102150405"))
+
+		err = ZipFiles(output, zipFiles)
+		if err != nil {
+			r.Status = http.StatusInternalServerError
+			r.Msg = "zip error"
+			return c.JSON(http.StatusInternalServerError, r)
+		}
+
+		r.Status = http.StatusOK
+		r.Msg = "OK"
+		r.Url = strings.Replace(output, ".", "", 1)
+		r.NotFound = notFound
+		return c.JSON(http.StatusOK, r)
+	}
+}
 
 func HandleIDImageUpload(conf *config.Config) func(c echo.Context) error {
 	type (
@@ -129,4 +204,57 @@ func mergeImg(path1, path2, path3 string) error {
 	dst = imaging.Paste(dst, src2Fit, image.Pt(10, 320))
 
 	return imaging.Save(dst, path3)
+}
+
+// ZipFiles compresses one or many files into a single zip archive file.
+// Param 1: filename is the output zip file's name.
+// Param 2: files is a list of files to add to the zip.
+func ZipFiles(filename string, files []string) error {
+
+	newZipFile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer newZipFile.Close()
+
+	zipWriter := zip.NewWriter(newZipFile)
+	defer zipWriter.Close()
+
+	// Add files to zip
+	for _, file := range files {
+
+		zipfile, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		defer zipfile.Close()
+
+		// Get the file information
+		info, err := zipfile.Stat()
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		// Using FileInfoHeader() above only uses the basename of the file. If we want
+		// to preserve the folder structure we can overwrite this with the full path.
+		header.Name = file
+
+		// Change to deflate to gain better compression
+		// see http://golang.org/pkg/archive/zip/#pkg-constants
+		header.Method = zip.Deflate
+
+		writer, err := zipWriter.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+		if _, err = io.Copy(writer, zipfile); err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- route.go
+++ route.go
@@ -21,6 +21,7 @@ func route(e *echo.Echo, conf *config.Config) (err error) {
 	e.GET("/", handler.HandleWelcome())
 	e.GET("/id_check", handler.HandleIDCheck(conf.IDCheckAPI))
 	e.POST("/upload", handler.HandleIDImageUpload(conf))
+	e.POST("/download", handler.HandleIDImageDownload(conf))
 
 	return
 }
