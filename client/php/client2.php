86209f649732c21b2236b79106fa8aa54ecd9859
lpisces <iamalazyrat@gmail.com>
2018-10-10 14:57:09 +0800

    fixed client2

--- client/php/client2.php
+++ client/php/client2.php
@@ -1,37 +1,40 @@
 <?php
 require("./vendor/autoload.php");
-
 $key = "";
 $secret = "";
-//$url = "https://test-idcheck.nle-tech.com/";
 $url = "http://192.168.0.131:1323";
-
 $id = "012345678912345678";
 $expire = time() + 5;
-
-//$orig = $secret . "expire=".$expire."&key=".$key."&name=".urlencode($name)."&number=".$number;
-$orig = $secret . "id=".$id;
+$orig = $secret . "expire=".$expire ."&id=".$id . "&key=".$key;
 echo $orig;
 echo "\n";
 $sign = md5($orig);
 echo $sign;
 echo "\n";
-
 $client = new GuzzleHttp\Client();
-$front = fopen("./012345678912345678_front.jpg", "r");
-$back = fopen("./012345678912345678_back.jpg", "r");
-$res = $client->request('POST', $url . '/upload', 
-	[ 
-		"query" => [
-			"id" => $id,
-			"expire" => $expire,
-			"key" => $key,
-			"sign" => $sign,
-		],
-		"front" => $front,
-		"back" => $back,
-	]
+$front = fopen("/root/idcheck/012345678912345678_front.jpg", "r");
+$back = fopen("/root/idcheck/012345678912345678_back.jpg", "r");
+$res = $client->request('POST', $url . '/upload',
+  [
+    "query" => [
+      "id" => $id,
+      "expire" => $expire,
+      "key" => $key,
+      "sign" => $sign,
+    ],
+    'multipart' => [
+      [
+        'name' => 'front',
+        'contents' => $front,
+        'filename' => '/root/idcheck/012345678912345678_front.jpg',
+      ],
+      [
+        'name' => 'back',
+        'contents' => $back,
+        'filename' => '/root/idcheck/012345678912345678_back.jpg',
+      ],
+    ]
+  ]
 );
-
 echo $res->getBody();
-echo "\n";
+echo "\n"
